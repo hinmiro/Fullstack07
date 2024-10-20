fff2aa39ddfdd99bed6edf491243ffe08276e300
mirohi <m_hintikka@hotmail.com>
2024-10-20 19:53:19 +0300

    Add assignment 7.7

--- country-hook/src/App.jsx
+++ country-hook/src/App.jsx
@@ -11,14 +11,35 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+        )
+        const countryObject = {
+          name: res.data.name.common,
+          capital: res.data.capital[0],
+          population: res.data.population,
+          flag: res.data.flags.svg,
+        }
+        console.log(res.data)
+        setCountry({ data: countryObject, found: true })
+      } catch (error) {
+        setCountry({ found: false })
+      }
+    }
+    if (name) {
+      fetchData()
+    }
+  }, [name])
 
   return country
 }
@@ -29,19 +50,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
